e9e59ee391e19c3a9103c4032e851d4ae39e5395
Shirleen <wambuimunk@gmail.com>
2025-12-03 02:28:37 +0300

    add static

--- backend/ecommerce/settings.py
+++ backend/ecommerce/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-i&sd^ev!iqa##=$0b3c3nv67ue_lidbj2kxhj_^klrmn3dp)a1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -124,6 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
