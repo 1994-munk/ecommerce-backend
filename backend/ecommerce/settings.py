747ff458c20079d57639422e20f364755b5a7cc6
Shirleen <wambuimunk@gmail.com>
2025-12-03 02:07:47 +0300

    feat:add sqlite3 in setting

--- backend/ecommerce/settings.py
+++ backend/ecommerce/settings.py
@@ -82,13 +82,9 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME'),  # from Docker env
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASS'),
-        'HOST': os.environ.get('DB_HOST'),
-        'PORT': '5432',
+     'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / "db.sqlite3",  # this will create a db.sqlite3 file in your project
     }
 }
 
