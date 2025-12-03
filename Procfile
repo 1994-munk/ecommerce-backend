d9ae4d14d7f126596fbf5c4272ef56a4cac3dd48
Shirleen <wambuimunk@gmail.com>
2025-12-03 12:57:02 +0300

    Fix wsgi import path

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn ecommerce.wsgi
+web: gunicorn backend.ecommerce.wsgi
--- backend/ecommerce/settings.py
+++ backend/ecommerce/settings.py
@@ -75,8 +75,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'ecommerce.wsgi.application'
-
+WSGI_APPLICATION = 'backend.ecommerce.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
